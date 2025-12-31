41263ccf26fe3dac2e751c2760a63d6fbdad37f6
Yu Sun <ysun@sctmes.com>
2025-12-31 14:56:44 +0800

    fix(hyprland): disable hardware cursors and set wayland envs via HM settings

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -9,10 +9,19 @@
       recursive = true;
     };
   };
-  
+
   wayland.windowManager.hyprland = {
     systemd.enable = false;
     enable = true;
     extraConfig = builtins.readFile ./hyprland.conf;
+    settings = {
+      cursor = {
+        no_hardware_cursors = true;
+      };
+      env = [
+        "NIXOS_OZONE_WL,1"
+        "WLR_RENDERER_ALLOW_SOFTWARE,1"
+      ];
+    };
   };
 }
