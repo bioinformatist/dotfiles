0e8b6dff54109c2733b6d547dbdb50e72c6c0c0f
Yu Sun <ysun@sctmes.com>
2026-01-23 17:15:12 +0800

    feat(network): enable system proxy

--- hosts/vm-test/configuration.nix
+++ hosts/vm-test/configuration.nix
@@ -59,8 +59,8 @@
   time.timeZone = "Asia/Shanghai";
 
   # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.proxy.default = "http://127.0.0.1:7897";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
