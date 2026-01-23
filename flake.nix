b2588d34a58c451e8682da3e997167093218f42e
Yu Sun <ysun@sctmes.com>
2026-01-23 16:19:09 +0800

    fix: restore missing sops-nix identifier in flake.nix

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
     swww.url = "github:LGFae/swww";
     yazi.url = "github:sxyazi/yazi";
     impermanence.url = "github:nix-community/impermanence";
+    sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
