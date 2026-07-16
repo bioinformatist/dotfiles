50bfe661c5ce9168b8e03e90f5833abf46010d8e
Yu Sun <ysun@sctmes.com>
2026-07-16 14:25:22 +0800

    fix: authorize Eww power profile switching

--- hosts/common/personal-workstation.nix
+++ hosts/common/personal-workstation.nix
@@ -1,4 +1,13 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   dotfiles.workstation.clash.enable = lib.mkDefault true;
+
+  security.polkit.extraConfig = lib.mkIf config.services.power-profiles-daemon.enable ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.UPower.PowerProfiles.switch-profile" &&
+          subject.local && subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
