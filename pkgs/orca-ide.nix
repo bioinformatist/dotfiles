522bea3245c07757707f45531484febeb4b9cc1f
Yu Sun <suny226@alumni.sysu.edu.cn>
2026-08-15 13:01:24 +0800

    chore(maint): update orca (#131)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkgs/orca-ide.nix
+++ pkgs/orca-ide.nix
@@ -6,8 +6,8 @@
 
 let
   pname = "orca-ide";
-  orcaVersion = "1.4.182";
-  orcaHash = "sha256-waC5UCpfbAFBBPtRt9Qgcx4d0bzw08Y/IDV+7lL3T2o=";
+  orcaVersion = "1.4.183";
+  orcaHash = "sha256-3PL9dKV/4S8hBoHmtQSNC3PT+/llCFY7BuWwnAE/0T4=";
 
   src = fetchurl {
     url = "https://github.com/stablyai/orca/releases/download/v${orcaVersion}/orca-linux.AppImage";
